d5d8baca7144721bcb4f0ff3de10a62f5325e4a6
Ksous4 <kelvinailson.000@gmail.com>
2025-02-24 20:01:00 -0300

    db inital files

--- db/db.py
+++ db/db.py
@@ -1,1 +1,9 @@
-#set conn here
+from sqlmodel import SQLModel, create_engine, Session
+
+DATABASE_URL = "postgresql+psycopg2://user:pass@localhost/logs"
+engine = create_engine(DATABASE_URL, echo=True)
+SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
