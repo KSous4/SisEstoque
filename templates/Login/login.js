b83d7505e3cb73e9813af6a50662820b04f82e99
Miguel Isper <miguelisperfilo@gmail.com>
2025-04-18 14:46:11 -0300

    1 - js
    teste para js

---
+++ templates/Login/login.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const loginForm = document.getElementById('loginForm');
+  
+    loginForm.addEventListener('submit', async (e) => {
+      e.preventDefault();
+  
+      const email = document.getElementById('email').value;
+      const senha = document.getElementById('senha').value;
+  
+      try {
+        const response = await fetch('http://localhost:3000/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ email, senha }),
+        });
+  
+        if (!response.ok) {
+          throw new Error('Login falhou');
+        }
+  
+        const data = await response.json();
+        console.log('Login bem-sucedido:', data);
+  
+  
+        alert(Bem-vindo(a), ${data.nome || 'usuário'}!);
+  
+  
+      } catch (error) {
+        console.error('Erro ao fazer login:', error);
+        alert('Email ou senha inválidos!');
+      }
+    });
+  });
